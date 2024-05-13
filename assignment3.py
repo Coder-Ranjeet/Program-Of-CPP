c934b1158b123128634297132f21a7a362f56fd3
Ranjeet Gupta <152593201+Coder-Ranjeet@users.noreply.github.com>
2024-05-13 14:19:33 +0530

    Python assignment part-3

---
+++ assignment3.py
@@ -0,0 +1,36 @@
+# for i in range(0,5): 
+#  print("My SirG",i)
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(1,11)[::-1]:
+#     print(i)
+
+# for i in range(1,20):
+#     if(i%2==1):
+#      print(i)
+
+# for i in range(1,20)[::-1]:
+#     if(i%2==1):
+#         print(i)
+
+# for i in range(0,20):
+#     if(i%2==0):
+#         print(i)
+
+# for i in range(0,20)[::-1]:
+#     if(i%2==0):
+#         print(i)
+
+# for i in range(1,11):
+#         print(i*i)
+
+# for i in range(1,11):
+#         print(i*i*i)
+
+for i in range(1,11):
+        print(i*5)
+    
+    
+    

fd1646646498010996348e1260536473f0f47c95
Ranjeet Gupta <152593201+Coder-Ranjeet@users.noreply.github.com>
2024-05-13 14:30:20 +0530

    Assign the comment in the program assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,34 +1,44 @@
-# for i in range(0,5): 
-#  print("My SirG",i)
+#This program print my sirG 5 times on the screen.
+for i in range(0,5): 
+ print("My SirG",i)
 
-# for i in range(1,11):
-#     print(i)
+#This program print 10 natural number.
+for i in range(1,11):
+    print(i)
 
-# for i in range(1,11)[::-1]:
-#     print(i)
+#This program print 10 natural number in reverse order.
+for i in range(1,11)[::-1]:
+    print(i)
 
-# for i in range(1,20):
-#     if(i%2==1):
-#      print(i)
+#This program print 10 odd natural number.
+for i in range(1,20):
+    if(i%2==1):
+     print(i)
 
-# for i in range(1,20)[::-1]:
-#     if(i%2==1):
-#         print(i)
+#This program print 10 odd natural number in reverse order.
+for i in range(1,20)[::-1]:
+    if(i%2==1):
+        print(i)
 
-# for i in range(0,20):
-#     if(i%2==0):
-#         print(i)
+#This program print 10 even natural number.
+for i in range(0,20):
+    if(i%2==0):
+        print(i)
 
-# for i in range(0,20)[::-1]:
-#     if(i%2==0):
-#         print(i)
+#This program print 10 even natural number in reverse order.
+for i in range(0,20)[::-1]:
+    if(i%2==0):
+        print(i)
 
-# for i in range(1,11):
-#         print(i*i)
+#This program print square of 10 natural number.
+for i in range(1,11):
+        print(i*i)
 
-# for i in range(1,11):
-#         print(i*i*i)
+#This program print the cubes of 1st 10 natural number.
+for i in range(1,11):
+        print(i*i*i)
 
+#This program print the multiplication of 5.
 for i in range(1,11):
         print(i*5)
     
